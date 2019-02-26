882fae4e607d17f9a9b740f3ad2de119c7371b7f
shhshelper <46355473+shhshelper@users.noreply.github.com>
2019-02-26 13:11:21 -0500

    Create mario1.js

---
+++ mario1.js
@@ -0,0 +1,25 @@
+var time=Date.now();document.onreadystatechange=function(a){if("complete"===a.target.readyState){var b=new UserWrappr.UserWrappr(FullScreenMario.FullScreenMario.prototype.proliferate({GameStartrConstructor:FullScreenMario.FullScreenMario},FullScreenMario.FullScreenMario.settings.ui,!0));console.log("It took "+(Date.now()-time)+" milliseconds to start."),b.displayHelpMenu()}};
+//# sourceMappingURL=index.min.js.map
+/*
+     FILE ARCHIVED ON 19:28:23 Mar 28, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:08:15 Feb 26, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 144.213 (3)
+  esindex: 0.015
+  captures_list: 283.827
+  CDXLines.iter: 13.325 (3)
+  PetaboxLoader3.datanode: 247.35 (5)
+  exclusion.robots: 119.383
+  xauthn.chkprivs: 0.038
+  exclusion.robots.policy: 119.365
+  RedisCDXSource: 3.274
+  PetaboxLoader3.resolve: 78.913 (2)
+  xauthn.identify: 119.094
+  load_resource: 205.878
+*/
